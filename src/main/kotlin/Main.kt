c7d4ce07638018c6f14db97f0c146fe222e160c5
Kyle "1ncredibr0" Swygert <kyleswygert@gmail.com>
2021-11-11 14:42:31 -0800

    Added MainKt initial file.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+/**/
+
+// Import statements for classes to be used.
+
+// Main function to execute program.
+fun main() {
+
+    // Note: This file will be where the main functionality of the program is executed.
+
+    for (i in 0..42 step 2) {
+        println(i)
+    }
+
+}
